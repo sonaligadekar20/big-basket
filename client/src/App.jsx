ba982a6c779c76ef81764acd18b7787dfc1ca1f7
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-08-08 21:29:45 +0530

    added light & dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import { Route, Routes } from "react-router-dom"
 function App() {
   return (
     <>
+    <div className='dark:bg-slate-900 dark:text-white'>
     <Routes>
       <Route path="/" element={<Home/>}/>
 
     </Routes>
+    </div>
     
     </>
   )
