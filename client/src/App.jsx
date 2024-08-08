7ba88de14d9d20d171c988ce3e455e90ea069d21
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-08-08 21:00:45 +0530

    install react-router-dom & created routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
+import React from 'react'; 
+import Home from "./home/Home"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Banner/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+
+    </Routes>
+    
     </>
   )
 }
---
+++ client/src/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    </>
+  )
+}
+
+export default Home
