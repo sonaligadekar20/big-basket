8b2827bb252fef3041601ebd9e265336ac614e1c
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-08-11 19:01:56 +0530

    created all product api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import User from "./models/User.js";
+import Product from "./models/product.js";
 dotenv.config();
 
 const app = express();
@@ -19,8 +20,6 @@ const connectDB = async () => {
     }
 };
 connectDB();
-
-
 
 // POST signup
 app.post("/signup", async (req, res) => {
@@ -76,6 +75,106 @@ app.post("/login", async (req, res) => {
     }
 });
 
+//   Get products
+
+// const products = [];
+app.get('/products', async (req, res) => {
+    const products = await Product.find();
+
+    res.json({
+        success: true,
+        data: products,
+        message: "Successfully featched all products."
+    })
+});
+
+// Post product
+app.post('/product', async (req, res) => {
+    const { name, price, image, category, brand } = req.body;
+
+    const product = new Product({
+        name: name,
+        price: price,
+        image: image,
+        category: category,
+        brand: brand
+    });
+    try {
+        const saveProduct = await product.save();
+        res.json({
+            success: true,
+            data: saveProduct,
+            message: "Successfully added new product"
+        })
+    }
+    catch (e) {
+        res.json({
+            success: false,
+            message: e.message
+        })
+    }
+});
+
+// Get product by id
+app.get('/product/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+    res.json({
+        success: true,
+        data: product,
+        message: "Get details of products."
+    })
+});
+
+// Delete product by id
+app.delete('/product/:id', async (req, res) => {
+    const { id } = req.params;
+    await Product.deleteOne({ _id: id });
+
+    res.json({
+        success: true,    
+        message: `Successfully deleted product with id ${id}`,
+    })
+});
+
+// Put product
+app.put('/product/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const { name, price, image, category, brand } = req.body;
+
+    await Product.updateOne({ _id: id },
+        {
+            $set: {
+                name: name,
+                price: price,
+                image: image,
+                category: category,
+                brand: brand
+            }
+        });
+
+    const updatedProduct = await Product.findById(id);
+    res.json({
+        success: true,
+        data: updatedProduct,
+        message: "Product updated successfully."
+    })
+});
+
+// GET /products/search?q=Sam
+app.get("/products/search", async (req, res) => {
+    const { q } = req.query;
+
+    const products = await Product.find({ name: { $regex: q, $options: "i" } });
+
+    res.json({
+        success:true,
+        data: products,
+        message: "Products fetched successfully"
+    });
+});
 
 
 const PORT = process.env.PORT || 4000;
